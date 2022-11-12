5220aefe067a38d93f630d8aa4d00a9aedaccef8
Surya T N <suryatej.nim@yahoo.com>
2022-11-12 12:33:09 +0000

    Implemented styled components for JSX elements in GradientDirectionItem component

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -1,1 +1,21 @@
-// Style your elements here
+import styled from 'styled-components'
+
+export const GradientDirectionItemContainer = styled.li`
+  background-color: #ffffff79;
+  color: #1e293b;
+  width: 20%;
+  margin: 0.15rem;
+  opacity: ${props => (props.isSelected ? 1 : 0.5)};
+  @media (min-width: 768px) {
+    width: 45%;
+  }
+`
+
+export const GradientDirectionItemButton = styled.button`
+  background: inherit;
+  border: none;
+  border-radius: 0.6rem;
+  outline: none;
+  cursor: pointer;
+  padding: 1rem;
+`
