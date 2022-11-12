e7a25bb3094376d0fb3c695b23066fce8c7bb700
Surya T N <suryatej.nim@yahoo.com>
2022-11-12 13:18:49 +0000

    Implemented JSX element tree in GradientDirectionItem component to render the intended UI, with click event handler and imported styled components

--- src/components/GradientDirectionItem/index.js
+++ src/components/GradientDirectionItem/index.js
@@ -1,1 +1,21 @@
-// Write your code here
+import {
+  GradientDirectionItemContainer,
+  GradientDirectionItemButton,
+} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {itemData, isItemSelected, onDirectionItemSelection} = props
+  const {value, displayText} = itemData
+
+  const onDirectionButtonClick = () => onDirectionItemSelection(value)
+
+  return (
+    <GradientDirectionItemContainer isSelected={isItemSelected}>
+      <GradientDirectionItemButton onClick={onDirectionButtonClick}>
+        {displayText}
+      </GradientDirectionItemButton>
+    </GradientDirectionItemContainer>
+  )
+}
+
+export default GradientDirectionItem
