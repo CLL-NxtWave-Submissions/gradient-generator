efe1af11e3036b99c9c913e699724eb799d55b44
Surya T N <suryatej.nim@yahoo.com>
2022-11-12 11:04:25 +0000

    Implemented bare-bones class component GradientGenerator with required imports and class syntax

--- src/components/GradientGenerator/index.js
+++ src/components/GradientGenerator/index.js
@@ -1,7 +1,25 @@
+import {Component} from 'react'
+
+import {
+  GradientGeneratorBgContainer,
+  GradientGeneratorHeader,
+  GradientDirectionSelectionContainer,
+  GradientGeneratorSubSectionText,
+  GradientDirectionsList,
+  GradientColorSelectionContainer,
+  GradientColorPickersList,
+  GradientColorPickerContainer,
+  GradientColorPicker,
+  GradientGeneratorButton,
+} from './styledComponents'
+
 const gradientDirectionsList = [
   {directionId: 'TOP', value: 'top', displayText: 'Top'},
   {directionId: 'BOTTOM', value: 'bottom', displayText: 'Bottom'},
   {directionId: 'RIGHT', value: 'right', displayText: 'Right'},
   {directionId: 'LEFT', value: 'left', displayText: 'Left'},
 ]
-// Write your code here
+
+export default class GradientGenerator extends Component {
+  state = {}
+}
