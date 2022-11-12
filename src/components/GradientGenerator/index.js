fabd0be96afdaca935abcf71e1e17c7b26d51ff1
Surya T N <suryatej.nim@yahoo.com>
2022-11-12 12:11:45 +0000

    Implemented the JSX element tree in GradientGenerator component with appropriate event handlers and imported styled components

--- src/components/GradientGenerator/index.js
+++ src/components/GradientGenerator/index.js
@@ -13,6 +13,8 @@ import {
   GradientGeneratorButton,
 } from './styledComponents'
 
+import GradientDirectionItem from '../GradientDirectionItem'
+
 const gradientDirectionsList = [
   {directionId: 'TOP', value: 'top', displayText: 'Top'},
   {directionId: 'BOTTOM', value: 'bottom', displayText: 'Bottom'},
@@ -21,5 +23,68 @@ const gradientDirectionsList = [
 ]
 
 export default class GradientGenerator extends Component {
-  state = {}
+  state = {
+      oldGradientColor1: #8ae323,
+      oldGradientColor2: #014f7b,
+      newGradientColor1: #8ae323,
+      newGradientColor2: #014f7b,
+  }
+
+  onColorChange = (colorChangeEvent) => {
+      const chosenColor = colorChangeEvent.target.value;
+      const colorPickerElementId = colorChangeEvent.target.id;
+
+      const updatedColorState = {}
+      updatedColorState[colorPickerElementId] = chosenColor
+
+      this.setState(updatedColorState)
+  }
+
+  onGenerateGradient = (generateGradientClickEvent) => {
+      generateGradientClickEvent.preventDefault()
+
+      this.setState(previousColorState => {
+          const {newGradientColor1, newGradientColor2} = previousColorState
+
+          return {
+              oldGradientColor1: newGradientColor1,
+              oldGradientColor2: newGradientColor2,
+          }
+      })
+  }
+
+  render(){
+      const {oldGradientColor1, oldGradientColor2, newGradientColor1, newGradientColor2} = this.state
+
+      return (
+          <GradientGeneratorBgContainer gradientColor1={oldGradientColor1} gradientColor2={oldGradientColor2}>
+              <GradientGeneratorHeader>Generate a CSS Color Gradient</GradientGeneratorHeader>
+
+              <GradientDirectionSelectionContainer>
+                <GradientGeneratorSubSectionText>Choose Direction</GradientGeneratorSubSectionText>
+                <GradientDirectionsList>
+                    {
+                    gradientDirectionsList.map(gradientDirectionsListItem => <GradientDirectionItem key={gradientDirectionsListItem.directionId} itemData={gradientDirectionsListItem} />)
+                }
+                </GradientDirectionsList>
+              </GradientDirectionSelectionContainer>
+
+              <GradientColorSelectionContainer onSubmit={this.onGenerateGradient}>
+                  <GradientGeneratorSubSectionText>Pick the Colors</GradientGeneratorSubSectionText>
+                  <GradientColorPickersList>
+                      <GradientColorPickerContainer>
+                          <GradientGeneratorSubSectionText>{newGradientColor1}</GradientGeneratorSubSectionText>
+                          <GradientColorPicker id="newGradientColor1" type="color" value={newGradientColor1} onChange={this.onColorChange} />
+                      </GradientColorPickerContainer>
+
+                      <GradientColorPickerContainer>
+                          <GradientGeneratorSubSectionText>{newGradientColor2}</GradientGeneratorSubSectionText>
+                          <GradientColorPicker id="newGradientColor2" type="color" value={newGradientColor2} onChange={this.onColorChange} />
+                      </GradientColorPickerContainer>
+                  </GradientColorPickersList>
+                  <GradientGeneratorButton type="submit">Generate</GradientGeneratorButton>
+              </GradientColorSelectionContainer> 
+          </GradientGeneratorBgContainer>
+      )
+  }
 }
